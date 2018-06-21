f30db1ec6cb5907e201331c000be67336c1753f2
Rylan <rylans@gmail.com>
2018-06-21 11:03:49 -0700

    Demo app with Photon

---
+++ js/menu.js
@@ -0,0 +1,56 @@
+const {shell} = require('electron')
+const os = require('os')
+var {remote } = require('electron')
+const {Menu, MenuItem} = remote
+//var Menu = remote.require('menu')
+//var MenuItem = remote.require('menu-item')
+
+// Build our new menu
+var menu = new Menu()
+menu.append(new MenuItem({
+  label: 'Delete',
+  click: function() {
+    // Trigger an alert when menu item is clicked
+    alert('Deleted')
+  }
+}))
+menu.append(new MenuItem({
+  label: 'More Info...',
+  click: function() {
+    // Trigger an alert when menu item is clicked
+    alert('Here is more information')
+  }
+}))
+
+const notification = {
+  title: 'Basic Notification',
+  body: 'Short message part'
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var nodeList = document.querySelectorAll('.js-context-menu')
+
+  Array.prototype.forEach.call(nodeList, function (item) {
+    item.addEventListener('contextmenu', function (event) {
+      console.log(event)
+      menu.popup(remote.getCurrentWindow());
+    })
+  })
+
+  const fileManagerBtn =document.getElementById('manage-files')
+  fileManagerBtn.addEventListener('click', (event) => {
+    shell.showItemInFolder(os.homedir())
+  });
+
+  const notificationButton = document.getElementById('notif');
+  notificationButton.addEventListener('click', () => {
+    const myNotification = new window.Notification(notification.title, notification)
+
+    myNotification.onclick = () => {
+      console.log('Notification clicked')
+    }
+  })
+
+
+})
+
---
+++ js/nav.js
@@ -0,0 +1,50 @@
+function hideAllPanes() {
+  var nodeList = document.querySelectorAll('.pane-view');
+  for(var i = 0; i < nodeList.length; i++){
+    nodeList[i].style.display = 'none';
+  }
+}
+
+function showHomeView() {
+  const homeNode = document.getElementById('view-home')
+  homeNode.style.display = 'block';
+}
+
+function showDownloadsView() {
+  const node  = document.getElementById('view-downloads');
+  node.style.display = 'block';
+}
+
+function showDocumentsView() {
+  const node  = document.getElementById('view-documents');
+  node.style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var nodeList = document.querySelectorAll('.pane-view');
+  console.log(nodeList)
+
+  hideAllPanes();
+  showHomeView();
+
+  const homeBtn = document.getElementById('btn-home');
+  const downloadBtn = document.getElementById('btn-downloads');
+  const documentsBtn = document.getElementById('btn-documents');
+
+  homeBtn.addEventListener('click', () => { 
+    hideAllPanes();
+    showHomeView();
+  });
+
+  downloadBtn.addEventListener('click', () => {
+    hideAllPanes();
+    showDownloadsView();
+  });
+
+  documentsBtn.addEventListener('click', () => {
+    hideAllPanes();
+    showDocumentsView();
+  });
+
+
+});
