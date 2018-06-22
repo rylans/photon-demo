d2cd56d0d1c981aa9143853914e3939d85ef8b15
Rylan <rylans@gmail.com>
2018-06-22 08:48:06 -0700

    Add Spectron and Jasmine tests

---
+++ spec/userproviderSpec.js
@@ -0,0 +1,23 @@
+var userprovider = require('../js/main/userprovider');
+
+describe('User Provider', function() {
+  it('provides three users', function () {
+    expect(userprovider.listUsers().length).toEqual(3);
+  });
+
+  it('provides users with names', function() {
+    var users = userprovider.listUsers();
+    for(var i=0; i<users.length; i++) {
+      expect(users[i].name).not.toBeUndefined();
+      expect(users[i].name).not.toBeNull();
+    }
+  });
+
+  it('provides users with title', function() {
+    var users = userprovider.listUsers();
+    for(var i=0; i<users.length; i++) {
+      expect(users[i].title).not.toBeUndefined();
+      expect(users[i].title).not.toBeNull();
+    }
+  });
+});
